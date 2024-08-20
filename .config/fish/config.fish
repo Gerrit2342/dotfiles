9ff0f4aa7726115267c41cc065601622bee999b9
Gerrit Albert <g.albert@posteo.de>
2024-08-20 21:07:33 +0200

    added aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,19 @@ abbr -a gc git commit
 abbr -a gp git push
 abbr -a nv nvim
 abbr -a cc doas make install clean
+
+alias ls="ls -hN --color=auto --group-directories-first"
+alias grep="grep --color=auto"
+alias diff="diff --color=auto"
+
+alias cp="cp -iv" 
+alias mv="mv -iv" 
+alias rm="rm -vI" 
+alias bc="bc -ql" 
+alias rsync="rsync -vrPlu" 
+alias mkd="mkdir -pv" 
+alias yt="yt-dlp --embed-metadata -i"
+alias yta="yt -x -f bestaudio/best"
+alias ytt="yt --skip-download --write-thumbnail"
+alias ffmpeg="ffmpeg -hide_banner"
+
