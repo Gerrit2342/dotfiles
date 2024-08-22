7834a20d0333553f588c9d251ddf6b16b43579e0
Gerrit Albert <g.albert@posteo.de>
2024-08-22 10:52:07 +0200

    added colorizer plugin

--- .config/nvim/lua/lz.lua
+++ .config/nvim/lua/lz.lua
@@ -96,4 +96,5 @@ local cmp = require 'cmp'
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
   require('lspconfig')['texlab'].setup {}
- 
+
+ require'colorizer'.setup()
