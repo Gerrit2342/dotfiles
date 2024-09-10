95ff33884fd5c6ef48d0474d85b770f800dcdfff
Gerrit Albert <g.albert@posteo.de>
2024-09-10 09:34:13 +0200

    added macros

--- .config/nvim/lua/LuaSnip/all.lua
+++ .config/nvim/lua/LuaSnip/all.lua
@@ -11,19 +11,44 @@ local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
 
-return {
-  -- Example: how to set snippet parameters
-  require("luasnip").snippet(
-    { -- Table 1: snippet parameters
-      trig="hi",
-      dscr="An autotriggering snippet that expands 'hi' into 'Hello, world!'",
-      regTrig=false,
-      priority=100,
-      snippetType="autosnippet"
-    },
-    { -- Table 2: snippet nodes (don't worry about this for now---we'll cover nodes shortly)
-      t("Hello, world!"), -- A single text node
-    }
-    -- Table 3, the advanced snippet options, is left blank.
-  ),
+-- return {
+--   -- Example: how to set snippet parameters
+--   require("luasnip").snippet(
+--     { -- Table 1: snippet parameters
+--       trig="zb",
+--       dscr="Macht daraus zum Beispiel",
+--       regTrig=false,
+--       priority=100,
+--       snippetType="autosnippet"
+--     },
+--     { -- Table 2: snippet nodes (don't worry about this for now---we'll cover nodes shortly)
+--       t("z. B."), -- A single text node
+--     }
+--     -- Table 3, the advanced snippet options, is left blank.
+--   ),
+-- }
+-- 
+M = {
+
+s({trig = "LG", snippetType = "autosnippet"},
+  { t("Liebe Grüße") }
+),
+
+s({trig = "VG", snippetType = "autosnippet"},
+  { t("Viele Grüße") }
+),
+
+s({trig = "MFG", snippetType = "autosnippet"},
+  { t("Mit freundlichen Grüßen") }
+),
+
+s({trig = "GA", snippetType = "autosnippet"},
+  { t("Gerrit Albert") }
+),
+
+s({trig = "SIG", snippetType = "autosnippet"},
+  { t({"--", "Fachstudienberater Technik", " ", "0241/80 27166", " ", "RWTH Aachen", "Didaktik der Physik und Technik", "Modulbau Physik 2, Raum 003", "Otto-Blumenthal-Straße 20", "52074 Aachen"}) }
+),
 }
+
+return M
